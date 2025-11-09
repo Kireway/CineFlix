8918ab6b179f3f577098943f2ec77d34ce2a3de3
Kireway <erik.silva@germinare.org.br>
2025-11-09 19:08:11 -0300

    Teste 3 do funcionamento de JS

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -1,12 +1,12 @@
 function entrar(event) {
     event.preventDefault(); // impede o envio automático do formulário
-    
+
     const nome = document.getElementById('nomeUsuario').value.trim(); // Remove espaços em branco
     const senha = document.getElementById('senhaUsuario').value.trim();
 
     if (nome === "ErikGames" && senha == "erik1234" ||
         nome === "CarlosLindo" && senha == "carlinhos123") {
-        window.location.href = '/assets/pages/inicio.html';
+        window.location.href = '/CineFlix/assets/pages/inicio.html';
     } else {
         window.alert("Usuário ou senha inválido(os)!");
     }
