332036906f197f31b9e2587c2559910a0e1a03b8
Kireway <erik.silva@germinare.org.br>
2025-11-09 18:45:45 -0300

    Removendo códigos desnecessários no JS

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -2,13 +2,10 @@ function entrar() {
     const nome = document.getElementById('nomeUsuario').value.trim(); // Remove espaços em branco
     const senha = document.getElementById('senhaUsuario').value.trim();
 
-    if (nome === "ErikGames" && senha == "erik1234") {
-        window.location.href = 'principal.html';
+    if (nome === "ErikGames" && senha == "erik1234" ||
+        nome === "CarlosLindo" && senha == "carlinhos123") {
+        window.location.href = '/assets/pages/inicio.html';
     } else {
         window.alert("Usuário ou senha inválido(os)!");
     }
-}
-
-function resenhaUltimato() {
-    window.location.href = 'ultimato.html';
 }
